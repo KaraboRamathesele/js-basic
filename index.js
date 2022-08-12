fe434dc995a9c3f1fa5fe2dc2bf336dddb7c025e
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-08-12 20:46:31 +0200

    Loops
    - For of
    - For in

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-let a ='red';
-let b = 'blue';
+//Hour
+//If hour is between 6am and 12pm: Good Morning!
+// If it is between 12pm and 6pm: Good Afternoon!
+// Otherwise: Good Evening!
 
-let c = a;
-a = b;
-b = c;
+let hour = 10;
 
-console.log(a);
-console.log(b);
+if (hour >= 6 && hour < 12){
+    console.log('Good Morning!');
+}
+else if (hour >= 12 && hour < 18){
+    console.log('Good Afternoon');
+}
+else {
+    console.log('Good Evening!');
+}
 
+//Switch--case
 
+let role ='guest';
+
+switch (role) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+
+    case 'moderator':
+        console.log('Moderator User');
+        break;
+
+    default:
+        console.log('Unknown User');
+}
+
+// Loops
+
+//for-in
+const person = {
+    name: 'K',
+    age: 30
+};
+
+for (let key in person);
+console.log(key, person[key]);
+
+const colors = ['red', 'green', 'blue'];
+
+// for (let index in colors)
+// console.log(index, colors[index]);
+
+// for-of
+
+for (let color of colors)
+console.log(color);
 
