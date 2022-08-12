5fd75891b1d4a140417ceee77757d7928d11f771
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-08-12 17:33:03 +0200

    Functions

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+//Arrays
+let selectedColors = ['red', 'blue'];
+selectedColors[2];
+//return the number of elements in the array
+console.log(selectedColors.length);
 
-//This is my first Javascript code
-console.log("Hello World");
+
+//Functions - set of statements that perform a task or calculate a value
+// Performing a task
+ function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+ }
+
+ greet('Karabo', 'Ramathesele');
+
+ // Calculating a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
+
+//If a customerhas more than 100 point,
+//They are a 'gold' customer, otherwise,
+// they are a'silver' customer.
+
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
+
+
+
