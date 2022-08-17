6e35dd9325f58f8f8f1734216bd0b64661e25cb9
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-08-17 17:52:42 +0200

    Constructor Function

--- factoryFunctions.js
+++ factoryFunctions.js
@@ -1,6 +1,7 @@
 
 
 // Factory function
+// uses camelNotation
 // Create logic and avoid dupilication
 
 function createCircle(radius, location) {
@@ -19,3 +20,18 @@ console.log(circle1);
 
 const circle2 = createCircle(2);
 console.log(circle2);
+
+
+// Constructor Function
+// Uses PascalNotation
+// Constructs and creates objects
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1);
+
