6d88c0799b3e7ac54f92b275cf8f271c33cab5b0
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-08-19 23:01:43 +0200

    Until Exercise 3

---
+++ part1ObjectsExecise/objectEquality.js
@@ -0,0 +1,14 @@
+
+let address1 = new Address('Babinaphuti', 'Joburg', '14693');
+let address2 = new Address('Babinaphuti', 'Joburg', '14693');
+
+
+function Address(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+}
+
+function areEqual(address1, address2) {
+    
+}
