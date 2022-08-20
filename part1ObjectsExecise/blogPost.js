2efa1766404883ce09f196e5937001f6ac89d61c
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-08-20 13:02:56 +0200

    Blog post objects

---
+++ part1ObjectsExecise/blogPost.js
@@ -0,0 +1,22 @@
+
+// title
+// body
+// author
+// views
+// comments (author, body)
+// isLive
+
+let post = {
+    title: 'Kekari',
+    body: 'bebe',
+    author: 's',
+    views: 10,
+    comments: [
+        { author: 'a', body: 'b' },
+        { author: 'c', body: 'd' },
+    ],
+    isLive: true
+};
+
+
+console.log(post);
