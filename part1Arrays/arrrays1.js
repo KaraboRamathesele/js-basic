c536b8a8e4b98174f77f4273d25c62486e72a446
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-08-20 14:05:30 +0200

    Finding reference element in array

--- part1Arrays/arrrays1.js
+++ part1Arrays/arrrays1.js
@@ -15,7 +15,7 @@ console.log(numbers);
 numbers.splice(2, 0, 'a', 'b');
 
 
-// Finding elements in an array
+// Finding primitive elements in an array
 const nomoro = [1, 2, 3, 1, 2, 4];
 
 console.log(nomoro.indexOf(2, 3)); // fromIndex will start counting from that point (index)
@@ -24,3 +24,15 @@ console.log(nomoro.lastIndexOf(1));
 // returns true if value exists
 console.log(nomoro.indexOf(1) !== -1);
 console.log(nomoro.includes(1));
+
+// Reference array
+const courses = [
+    { id: 1, name: 'a' },
+    { id: 2, name: 'b' },
+];
+
+const course = courses.find(function(course) {
+    return course.name === 'a';
+});
+
+console.log(course);
